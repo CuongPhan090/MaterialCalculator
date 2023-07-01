c0ea57856d86c4efe9ba5a933eab1b68289bb6d5
CuongPhan090 <vcuong0909@gmail.com>
2023-07-01 14:43:16 -0400

    Built Calculation UI

--- app/src/main/java/com/example/materialcalculator/MainActivity.kt
+++ app/src/main/java/com/example/materialcalculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.materialcalculator.presentation.CalculatorScreen
 import com.example.materialcalculator.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialCalculatorTheme {
-
+                CalculatorScreen()
             }
         }
     }
